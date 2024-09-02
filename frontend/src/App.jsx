e464df05d447be9efb8910c18d1f35cc13507da3
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-09-02 09:46:39 +0530

    initial commit with templates

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">
+        Content Verification on Solana
+      </h1>
+    </div>
+  );
+}
+
+export default App;
